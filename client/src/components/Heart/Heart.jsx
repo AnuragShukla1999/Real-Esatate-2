7c0caf3206f882bca6812d9d629b48c36a235f4d
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-04-07 19:14:33 +0530

    .

--- client/src/components/Heart/Heart.jsx
+++ client/src/components/Heart/Heart.jsx
@@ -1,3 +1,21 @@
+import { useContext, useState } from "react";
+import { AiFillHeart } from "react-icons/ai"
 
+const Heart = ({ id }) => {
 
-const Heart = 
+    const [heartColor, setHeartColor] = useState("white");
+    const { validateLogin } = useAuthCheck();
+
+    const { user } = useAuth0();
+
+    const {
+        userDetails: {favourites, token},
+        setUserDetails,
+    } = useContext(UserDetailContext);
+
+    return (
+        <AiFillHeart/>
+    )
+};
+
+export default Heart;
